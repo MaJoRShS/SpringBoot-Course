e2b10774eb206df7f541eec44a8f86287df82d69
MaJoRShS <lukas.mr@hotmail.com>
2021-09-30 15:55:05 -0300

    Order, Instant, ISO 8601 - Part 1
    Here changed the class to recive the relationship with the new table order, changed the name off table , implements the list of orders and create the foreign  key using notatios and declare the direction of relationship

--- src/main/java/br/com/course/entities/User.java
+++ src/main/java/br/com/course/entities/User.java
@@ -1,14 +1,20 @@
 package br.com.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -19,6 +25,9 @@ public class User implements Serializable {
 	private String email;
 	private String phoneString;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
@@ -72,6 +81,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phoneString=" + phoneString + ", password="
@@ -94,5 +107,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 
 }
