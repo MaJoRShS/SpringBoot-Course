eb1290a11105ae36f94ed1feb20abe3f9d0ed145
MaJoRShS <lukas.mr@hotmail.com>
2021-10-03 21:23:08 -0300

    OrderItem, many-to-many association with extra attributes PART 2
    Here is big problem and I dont need this now

--- src/main/java/br/com/course/config/TestConfig.java
+++ src/main/java/br/com/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.course.entities.Category;
 import br.com.course.entities.Order;
+import br.com.course.entities.OrderItem;
 import br.com.course.entities.Product;
 import br.com.course.entities.User;
 import br.com.course.entities.enums.OrderStatus;
 import br.com.course.repositories.CategoryRepository;
+import br.com.course.repositories.OrderItemRepository;
 import br.com.course.repositories.OrderRepository;
 import br.com.course.repositories.ProductRepository;
 import br.com.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -60,15 +65,21 @@ public class TestConfig implements CommandLineRunner {
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		
+
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
