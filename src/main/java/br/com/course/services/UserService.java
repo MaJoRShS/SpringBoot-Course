5dde5cf08a45d530aeaea4ac97d1ef573fbddd57
MaJoRShS <lukas.mr@hotmail.com>
2021-10-09 15:15:02 -0300

    Exception handling - update
    Here I Create the same thing but now for the update

--- src/main/java/br/com/course/services/UserService.java
+++ src/main/java/br/com/course/services/UserService.java
@@ -13,6 +13,8 @@ import br.com.course.entities.User;
 import br.com.course.repositories.UserRepository;
 import br.com.course.services.exceptions.ResourceNotFoundException;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,14 @@ public class UserService {
 		}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updataData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updataData(entity, obj);
+			return repository.save(entity);
+		}catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
 
 	private void updataData(User entity, User obj) {
